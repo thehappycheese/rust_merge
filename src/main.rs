dafc951112bc2c4d57b96285d4e12f5178a78e87
thehappycheese <the.nicholas.archer@gmail.com>
2021-08-02 23:41:01 +0800

    took hours to find the cast_with_dtype function

--- src/main.rs
+++ src/main.rs
@@ -6,38 +6,52 @@ use mimalloc::MiMalloc;
 static GLOBAL: MiMalloc = MiMalloc;
 
 
-use polars::prelude::{self as pl, ChunkTake, SerReader};
-use polars::frame::DataFrame;
+use polars::prelude::*;
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+//use polars::frame::DataFrame;
+
+fn main() -> Result<()>{
     println!("Hello, world!");
-    let segments=  get_csv("./test_data/segments.csv")?;
-    println!("{:?}", segments);
+    let mut segments=  get_csv("")?;
+    segments.set_column_names(&["road","cway","seg_name","slk_from","slk_to","slk_length"])?;
+    
+    let slk_from = (segments.drop_in_place("slk_from")?.f64()?*1000.0).into_series().cast_with_dtype(&DataType::Int32)?;
+    let slk_to = (segments.drop_in_place("slk_to")?.f64()?*1000.0).into_series().cast_with_dtype(&DataType::Int32)?;
+    segments = segments.hstack(&[slk_from, slk_to])?;
 
-    let data=  get_csv("./test_data/data.csv")?;
+    println!("{:?}", segments);
+    println!("{:?}", segments.fields());
+    
+
+    let data=  get_csv("")?;
     println!("{:?}", data);
-    merge(segments, data);
+    println!("{:?}", data.fields());
+
+
+//     let data= LazyCsvReader::new("".into())
+//     .has_header(true)
+//     .with_delimiter(b',')
+//     .finish();
+// let result = data.sort("START_SLK", true).fetch(100)?;
+// print!("{:?}",result);
+
+    
+    //let _df = merge(segments, data);
     Ok(())
 }
 
 
-fn get_csv<PathType>(path:PathType) -> Result<DataFrame, Box<dyn std::error::Error>> 
+fn get_csv<PathType>(path:PathType) -> Result<DataFrame> 
     where PathType:Into<std::path::PathBuf>{
-    match pl::CsvReader::from_path(path)?.infer_schema(None).has_header(true).finish(){
-        Ok(df)=>Ok(df),
-        Err(e)=>{
-            println!("failed to load {}",e);
-            return Err(Box::new(e));
-        }
-    }
+    CsvReader::from_path(path)?.infer_schema(None).has_header(true).finish()
 }
 
-fn merge(target:DataFrame, data:DataFrame) -> Result<DataFrame, Box<dyn std::error::Error>>{
+fn merge(target:DataFrame, data:DataFrame) -> Result<DataFrame>{
     let data_group_indecies = data.groupby(&["road_no","cway"]).unwrap().groups().unwrap();
     
-    let target_group_by = target.groupby(&["road_no","cway"]).unwrap();
+    //let target_group_by = target.groupby(&["road_no","cway"]).unwrap();
 
-    target_group_by.
+
     
     data_group_indecies.column("groups").unwrap()
         .list().unwrap()
